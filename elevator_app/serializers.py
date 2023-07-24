b1acbe058dc3c2877742d414609424808b33eb0c
sknasirhussain <sknasirhussainofficial@gmail.com>
2023-07-25 01:47:32 +0530

    minor changes

--- elevator_app/serializers.py
+++ elevator_app/serializers.py
@@ -1,13 +1,21 @@
 from rest_framework import serializers
 from .models import Elevator
 from .models import userRequest
+from .models import ElevatorSystem
 
+#serializer for Elevator model
 class ElevatorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Elevator
         fields = '__all__'
 
+#serializer for ElevatorSystem model
+class ElevatorsystemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model  = ElevatorSystem
+        fields =  '__all__'
 
+#serializer for userRequest model
 class userRequestSerializer(serializers.ModelSerializer):
     class Meta:
         model = userRequest
--- elevator_app/urls.py
+++ elevator_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from elevator_app.views import ElevatorViewSet, userRequestViewSet
 
-# Create a router and register our ViewSets with it.
+# Created a router and registered the ViewSets with it.
 router = DefaultRouter()
 router.register(r'elevators', ElevatorViewSet)
 router.register(r'user-requests', userRequestViewSet)
